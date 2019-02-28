5f3bd5229a313f0417137b51eee760502136ba48
Ronald Glover <ron881212@gmail.com>
2019-02-27 21:47:52 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,35 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "ExcuseBarrel12",
-  database: "burgers_db"
-});
+var connection;
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "ExcuseBarrel12",
+//   database: "burgers_db"
+// });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+if(process.env.JAWSBD_URL){
+  connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "ExcuseBarrel12",
+    database: "burgers_db"
+  });
+}
 
 // Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;

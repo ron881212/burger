eb1823190483f1ecb7a4b6662215444702790a00
Ronald Glover <ron881212@gmail.com>
2019-02-28 09:52:48 -0500

    dubugging post routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
     burger.create([
-    "burger"
+    "burger_name"
   ], [
     req.body.name
   ], function(result) {
